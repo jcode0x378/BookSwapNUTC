d8d96f84294f16ea03498e0ed2be866f5d767d9b
jcode0x378 <jcode0x378@gmail.com>
2024-09-22 16:19:08 +0800

    Update project and connect to local MongoDB

--- server/server.js
+++ server/server.js
@@ -2,19 +2,31 @@ require('dotenv').config({ path: './server/.env' });
 console.log('MONGO_URI:', process.env.MONGO_URI);
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
+const cors = require('cors'); // 添加這行
 const bookRoutes = require('./routes/bookRoutes');
 const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 const port = process.env.PORT || 5001;
 
-// 中間件
+// 使用CORS中間件
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://5113-125-229-242-128.ngrok-free.app',
+];
+
 app.use(
   cors({
-    origin: 'http://localhost:5173', // 替換為你的前端地址
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
   }),
 );
+
 app.use(express.json());
 
 // 路由
